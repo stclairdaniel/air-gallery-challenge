e018f5b0d6c28bcad10a8f55bc6b00b8efb93056
Daniel St. Clair <stclair.daniel@gmail.com>
2025-03-12 18:57:58 -0700

    basic boards view

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "air-prod.imgix.net",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
--- src/app/ components/Boards.tsx
+++ src/app/ components/Boards.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { BoardsListResponse, fetchBoards } from "../api/boards";
+import Image from "next/image";
 
 export const Boards = () => {
   const [boards, setBoards] = useState<BoardsListResponse | null>(null);
@@ -12,5 +13,28 @@ export const Boards = () => {
     fetch();
   }, []);
 
-  return <>Boards</>;
+  return (
+    <>
+      Boards ({boards?.data?.length})
+      <div className="flex flex-wrap gap-4">
+        {boards?.data?.map((board) => (
+          <div
+            key={board.id}
+            className="text-12 font-bold uppercase text-grey-10 w-[200px] h-[200px] overflow-hidden"
+          >
+            {board.thumbnails?.[0] && (
+              <Image
+                src={board.thumbnails?.[0]}
+                alt={board.description || board.description || "boardImage"}
+                priority
+                width={400}
+                height={400}
+                className="grayscale"
+              />
+            )}
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
