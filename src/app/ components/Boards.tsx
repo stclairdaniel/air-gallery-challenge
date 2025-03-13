c39a836fa5154938bc52deadc886b64e53bddeaa
Daniel St. Clair <stclair.daniel@gmail.com>
2025-03-12 18:33:51 -0700

    revert next.config to js, add initial boards component

--- src/app/ components/Boards.tsx
+++ src/app/ components/Boards.tsx
@@ -1,3 +1,16 @@
+import { useEffect, useState } from "react";
+import { BoardsListResponse, fetchBoards } from "../api/boards";
+
 export const Boards = () => {
+  const [boards, setBoards] = useState<BoardsListResponse | null>(null);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const boardsResponse = await fetchBoards();
+      setBoards(boardsResponse);
+    };
+    fetch();
+  }, []);
+
   return <>Boards</>;
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Boards } from "./ components/Boards";
 
 export default function Home() {
